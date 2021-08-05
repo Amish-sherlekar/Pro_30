bd8f85b76e9942f51442f533850baadec49f6507
Amish-sherlekar <asmitasherlekar@gmail.com>
2021-08-05 21:52:29 +0530

    jss

---
+++ bob.js
@@ -0,0 +1,24 @@
+class Bob{
+
+   constructor(x,y,radius){
+      var option={
+         restitution:1,
+         density:0.4,
+         friction:0
+      }
+      
+      this.body= Bodies.circle(x,y,radius,option)
+      World.add(world,this.body)
+
+      this.radius=radius
+   }
+
+   display(){
+
+      push()
+      fill("pink")
+      ellipseMode(CENTER)
+      circle(this.body.position.x,this.body.position.y,this.radius*2)
+      pop()
+   }
+}
